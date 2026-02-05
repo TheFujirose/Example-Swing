8254d54d27a7f9c94dd8c71a6f1b606e0ada5749
TheFujirose <50027005+TheFujirose@users.noreply.github.com>
2026-02-05 16:20:02 -0500

    feat: website - thymetemplates using bootstrap - controller - model

--- src/main/java/com/example/portal/controller/StudentController.java
+++ src/main/java/com/example/portal/controller/StudentController.java
@@ -1,4 +1,62 @@
 package com.example.portal.controller;
 
+import com.example.portal.model.Student;
+import org.jspecify.annotations.Nullable;
+import org.springframework.beans.propertyeditors.StringTrimmerEditor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Controller
+@RequestMapping("/")
 public class StudentController {
+
+    /**
+     * List of students made.
+     * TODO: switch with SQLite
+     */
+    private static @Nullable List<Student> students = new ArrayList<Student>();
+
+    /**
+     * Preprocessing that removes whitespace. Will replace that space with null so
+     * there needs to be a handler for that exception
+     * @param binder
+     */
+    @InitBinder
+    public void initBinder(WebDataBinder binder) {
+        binder.registerCustomEditor(String.class, new StringTrimmerEditor(true));
+    }
+
+    /**
+     * Basic Index for binding example
+     * @return the string of the thymeleaf template in resources
+     */
+    @GetMapping
+    public String index(Model model){
+        model.addAttribute(new Student());
+        return "register";
+    }
+
+    @PostMapping("/submit")
+    public String submit(@ModelAttribute Student student, Model model){
+        if (students != null) { // This won't be here in later versions when I switch to SQL
+            students.add(student);
+        }
+        model.addAttribute("students", students);
+        return "confirm";
+    }
+
+    @ModelAttribute("courses")
+    public List<String> getLevels(){
+        return List.of("CPAN228","CPAN227","CPAN226","CPAN225","CPAN224","WORK111","WORK099");
+    }
+
+    @ModelAttribute("levels")
+    public List<String> getCourses(){
+        return List.of("university","college","trade");
+    }
 }
--- src/main/java/com/example/portal/model/Student.java
+++ src/main/java/com/example/portal/model/Student.java
@@ -1,4 +1,157 @@
 package com.example.portal.model;
 
+/**
+ * @author Carson Fujita
+ * @since 1.0.0
+ *
+ * Describes a model of a student in a school with properties defining their
+ * - name
+ * - email
+ * - course
+ * - level of study
+ * Additionally contains an id
+ */
 public class Student {
+
+    /**
+     * The enumerate identification number for the Student.
+     * @since 1.0.0
+     */
+    private int id;
+
+    /**
+     * The full-name of the Student.
+     * @since 1.0.0
+     */
+    private String name;
+
+    /**
+     * The primary email for the Student.
+     * @since 1.0.0
+     */
+    private String email;
+
+    /**
+     * The selected course for the Student.
+     * @since 1.0.0
+     */
+    private String course;
+
+    /**
+     * The level of study for the Student.
+     * <br/>
+     * Best described by:
+     * <ul>
+     *     <li>University</li>
+     *     <li>College</li>
+     *     <li>Trades</li>
+     * </ul>
+     * @since 1.0.0
+     */
+    private String level;
+
+    /**
+     * Gets the id of this Student
+     *
+     * @see Student#id
+     * @see Student#setId
+     */
+    public int
+    getId() {
+        return id;
+    }
+
+    /**
+     * Sets the id of this Student
+     *
+     * @param id the id to set
+     * @see Student#id
+     */
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    /**
+     * Gets the name of this Student
+     *
+     * @see Student#name
+     * @see Student#setName
+     */
+    public String
+    getName() {
+        return name;
+    }
+
+    /**
+     * Sets the name of this Student
+     *
+     * @param name the name to set
+     * @see Student#name
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    /**
+     * Gets the email of this Student
+     *
+     * @see Student#email
+     * @see Student#setEmail
+     */
+    public String
+    getEmail() {
+        return email;
+    }
+
+    /**
+     * Sets the email of this Student
+     *
+     * @param email the email to set
+     * @see Student#email
+     */
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    /**
+     * Gets the course of this Student
+     *
+     * @see Student#course
+     * @see Student#setCourse
+     */
+    public String
+    getCourse() {
+        return course;
+    }
+
+    /**
+     * Sets the course of this Student
+     *
+     * @param course the course to set
+     * @see Student#course
+     */
+    public void setCourse(String course) {
+        this.course = course;
+    }
+
+    /**
+     * Gets the level of this Student
+     *
+     * @see Student#level
+     * @see Student#setLevel
+     */
+    public String
+    getLevel() {
+        return level;
+    }
+
+    /**
+     * Sets the level of this Student
+     *
+     * @param level the level to set
+     * @see Student#level
+     */
+    public void setLevel(String level) {
+        this.level = level;
+    }
 }
